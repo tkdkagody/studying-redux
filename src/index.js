23ecbeef4bdcd06031c8ee83c487a88a8c89ecb3
tkdkagody <d_heeee2@naver.com>
2021-12-10 01:33:13 +0900

    add: store

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
+import { createStore } from "redux";
 
 
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+
+let count = 0;
+
+
+const updateText = () => {
+  number.innerText = count;
+}
+
+const handleAdd = () => {
+  count = count +1; 
+  updateText();
+}
+
+const handleMinus = () => {
+  count = count -1; 
+  updateText();
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus)
