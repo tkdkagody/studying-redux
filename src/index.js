128fd7cbcc7e02159c9f917fe87170400729dd9d
tkdkagody <d_heeee2@naver.com>
2021-12-10 02:34:05 +0900

    subscribe

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { createStore } from "redux";
 
-
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 
+number.innerText = 0; 
 
 const countModifier = (count = 0, action) => {  //only can modify data
+  console.log(count, action)
   if(action.type === "ADD"){
     return count +1 ;
   }else if(action.type === "MINUS"){
@@ -19,12 +20,22 @@ const countModifier = (count = 0, action) => {  //only can modify data
 
 const countStore = createStore(countModifier);  //data를 저장하는 곳 
 
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+countStore.subscribe(onChange);
 
-countStore.dispatch({ type : "ADD"});
-countStore.dispatch({ type : "ADD"})
-countStore.dispatch({ type : "ADD"})
-countStore.dispatch({ type : "MINUS"})
+const handleAdd = ( ) => {
+  countStore.dispatch({ type: "ADD"})
+}
 
-console.log(countStore.getState())
+const handleMinus = () => {
+  countStore.dispatch({type:"MINUS"})
+}
+
+
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 
